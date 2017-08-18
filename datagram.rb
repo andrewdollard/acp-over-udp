977090b41d606bda586aed239317a313abc3e70b
adollard <code@getbraintree.com>
2017-08-18 12:23:38 -0700

    simplify

--- datagram.rb
+++ datagram.rb
@@ -1,47 +1,25 @@
 class Datagram
 
-  attr_reader :source_ip, :source_port, :dest_ip, :dest_port,
-              :seq, :ack, :message
+  ATTRS = [ :source_ip, :source_port, :dest_ip, :dest_port,
+            :seq, :ack, :message ]
 
-  def initialize(args)
-    args.each do |k,v|
-      instance_variable_set("@#{k}", v) unless v.nil?
+  attr_reader *ATTRS
+
+  def initialize(params)
+    ATTRS.each do |attr|
+      instance_variable_set("@"+attr.to_s,params[attr])
     end
-  # end
-  #   params.keys.each do |key|
-  #     # this.
-  #   end
-  #
-  #   @source_ip = params[:source_ip]
-  #   @source_port = params[:source_port]
-  #   @dest_ip = params[:dest_ip]
-  #   @dest_port = params[:dest_port]
-  #   @seq = params[:seq]
-  #   @ack = params[:ack]
-  #   @message = params[:message]
   end
 
   def self.parse(msg)
-    raw = msg.split('|')
-    new({
-      source_ip: raw[0],
-      source_port: raw[1],
-      dest_ip: raw[2],
-      dest_port: raw[3],
-      seq: raw[4],
-      ack: raw[5],
-      message: raw[6]
-    })
+    params = msg.split('|')
+    attrs = ATTRS.map.each_with_index{ |a, i| [a, params[i]] }.to_h
+    new(attrs)
   end
 
   def serialize
-    [@source_ip,
-     @source_port,
-     @dest_ip,
-     @dest_port,
-     @seq,
-     @ack,
-     @message].join("|")
+    params = ATTRS.map { |attr| instance_variable_get("@#{attr}") }
+    params.join("|")
   end
 
 end
